bbea6b74ab859a6995fd73887385e05d92aa562b
LSP-Lucas <lucassppedroso@gmail.com>
2024-11-08 10:00:17 -0300

    add impl to contract findById user

--- src/main/java/br/com/curse/webflux/controller/impl/UserControllerImpl.java
+++ src/main/java/br/com/curse/webflux/controller/impl/UserControllerImpl.java
@@ -1,6 +1,7 @@
 package br.com.curse.webflux.controller.impl;
 
 import br.com.curse.webflux.controller.UserController;
+import br.com.curse.webflux.mapper.UserMapper;
 import br.com.curse.webflux.model.request.UserRequest;
 import br.com.curse.webflux.model.response.UserResponse;
 import br.com.curse.webflux.service.UserService;
@@ -18,6 +19,7 @@ import reactor.core.publisher.Mono;
 public class UserControllerImpl implements UserController {
 
     private final UserService service;
+    private final UserMapper mapper;
 
     @Override
     public ResponseEntity<Mono<Void>> save(UserRequest request) {
@@ -26,8 +28,8 @@ public class UserControllerImpl implements UserController {
     }
 
     @Override
-    public ResponseEntity<Mono<UserResponse>> find(String id) {
-        return null;
+    public ResponseEntity<Mono<UserResponse>> findById(String id) {
+        return ResponseEntity.ok().body(service.findById(id).map(mapper::toResponse));
     }
 
     @Override
--- src/main/java/br/com/curse/webflux/mapper/UserMapper.java
+++ src/main/java/br/com/curse/webflux/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package br.com.curse.webflux.mapper;
 
 import br.com.curse.webflux.entity.User;
 import br.com.curse.webflux.model.request.UserRequest;
+import br.com.curse.webflux.model.response.UserResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -17,4 +18,6 @@ public interface UserMapper {
 
     @Mapping(target = "id", ignore = true)
     User toEntity(final UserRequest request);
+
+    UserResponse toResponse(final User entity);
 }
