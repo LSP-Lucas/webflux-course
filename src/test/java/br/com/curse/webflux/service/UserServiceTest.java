d29f724fe3c3e903839ab499649b6f17a67e1de4
LSP-Lucas <lucassppedroso@gmail.com>
2024-11-13 11:02:44 -0300

    add unit test to findAll

--- src/test/java/br/com/curse/webflux/service/UserServiceTest.java
+++ src/test/java/br/com/curse/webflux/service/UserServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -50,7 +51,7 @@ class UserServiceTest {
     }
 
     @Test
-    void testFinfById() {
+    void testFindById() {
         when(repository.findById(anyString())).thenReturn(Mono.just(User.builder().build()));
 
         Mono<User> result = service.findById("123");
@@ -61,5 +62,19 @@ class UserServiceTest {
                 .verify();
 
         Mockito.verify(repository, times(1)).findById(anyString());
+    }
+
+    @Test
+    void testFindAll() {
+        when(repository.findAll()).thenReturn(Flux.just(User.builder().build()));
+
+        Flux<User> result = service.findAll();
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(repository, times(1)).findAll();
     }
 }
